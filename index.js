bafcd91abe7da7ce3a67caf89a3ad03ef213a42b
sclash <samuelpellikka@gmail.com>
2023-02-27 20:52:39 +0200

    Päivän ruokalista

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const url = "https://www.sodexo.fi/ruokalistat/output/weekly_json/152";
 const menuContainer = document.getElementById("menu-container");
-const restaurantNameElement = document.getElementById("restaurant-name");
+const weekdays = [
+  "Sunnuntai",
+  "Maanantai",
+  "Tiistai",
+  "Keskiviikko",
+  "Torstai",
+  "Perjantai",
+  "Lauantai"
+];
 
 fetch(url)
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     const restaurantName = data.meta.ref_title;
-    restaurantNameElement.textContent = restaurantName;
-    
     const mealDates = data.mealdates;
-    const today = new Date().toLocaleDateString("fi-FI");
-    
-    const todayMenu = mealDates.find(day => day.date === today);
-    if (!todayMenu) {
-      const noMenuMessage = document.createElement("p");
-      noMenuMessage.textContent = "Valitettavasti tälle päivälle ei löydy ruokalistaa";
-      menuContainer.appendChild(noMenuMessage);
-      return;
-    }
-    
-    const menuItems = todayMenu.courses;
+    const currentDate = new Date();
+    const currentWeekday = weekdays[currentDate.getDay()].toLowerCase();
+    const todaysLunchList = mealDates.find(
+      (e) => e.date.toLowerCase() === currentWeekday
+    );
+    const menuItems = todaysLunchList.courses;
+
     const menuList = document.createElement("ul");
-    
-    Object.keys(menuItems).forEach(key => {
+    menuList.innerHTML = `<h3>${currentWeekday}</h3>`;
+    console.log(menuList.innerHTML);
+    Object.keys(menuItems).forEach((key) => {
       const menuItem = menuItems[key];
       const menuItemElement = document.createElement("li");
       const menuItemName = menuItem.title_fi;
@@ -30,7 +33,7 @@ fetch(url)
       menuItemElement.textContent = `${menuItemName} (${menuItemProperties})`;
       menuList.appendChild(menuItemElement);
     });
-    
+    menuContainer.innerHTML = `<h2>${restaurantName}</h2>`;
     menuContainer.appendChild(menuList);
   })
-  .catch(error => console.log(error));
+  .catch((error) => console.log(error));
